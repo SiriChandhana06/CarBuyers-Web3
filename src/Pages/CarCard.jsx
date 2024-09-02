296110cd75136e8f22292c92575b1d67fce1f039
SiriChandhana06 <sirisiri3006@gmail.com>
2024-09-02 21:46:49 +0530

    UI Update

--- src/Pages/CarCard.jsx
+++ src/Pages/CarCard.jsx
@@ -47,14 +47,14 @@ const CarCard = () => {
   const handleSearch = (e) => {
     const query = e.target.value.toLowerCase();
     setSearchQuery(query);
-    
+
     if (query === '') {
       setFilteredData(data); // If the search query is empty, reset to show all data
     } else {
       const filtered = data.filter(item => {
         const priceInEther = ethers.utils.formatEther(item.price);
         const year = item.year ? item.year.toNumber() : item[1].toNumber();
-        
+
         return (
           item.title.toLowerCase().includes(query) ||
           item.model.toLowerCase().includes(query) ||
@@ -75,28 +75,39 @@ const CarCard = () => {
   return (
     <div>
       <div className="mb-6 flex justify-center overflow-x-hidden">
-  <div className="relative w-80">
-    <input
-      type="text"
-      value={searchQuery}
-      onChange={handleSearch}
-      placeholder="Search Here"
-      className="px-4 py-2 pl-12 border border-white/30 backdrop-blur-md bg-white/30 text-black placeholder:text-black rounded-full shadow-sm w-64 md:w-full"
-    />
-    <div className="absolute inset-y-0 left-4 flex items-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="black"
-      >
-        <path d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14" />
-      </svg>
-    </div>
-  </div>
-</div>
-      
+        <div className="relative w-80">
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearch}
+            placeholder="Search Here"
+            className="px-4 py-2 pl-12 border border-white/30 backdrop-blur-md bg-white/30 text-black placeholder:text-black rounded-full shadow-sm w-64 md:w-full"
+          />
+          <div className="absolute inset-y-0 left-4 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="black"
+            >
+              <path d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className='flex justify-center py-5'>
+          <div>
+            <button className='backdrop-blur-md bg-white/30 py-6 px-6 md:px-20 border border-white/30 rounded-l-xl shadow-lg text-2xl hover:text-gray-200 md:hover:text-gray-500 hover:scale-90 hover:rounded-xl'>All Posts</button>
+          </div>
+          <div>
+            <button className='backdrop-blur-md bg-white/30 py-6 px-6 md:px-20 border border-white/30 rounded-r-xl shadow-lg text-2xl hover:text-gray-200 md:hover:text-gray-500 hover:scale-90 hover:rounded-xl'>My Post</button>
+          </div>
+        </div>
+      </div>
+
+
       {filteredData.length === 0 ? (
         <div className="text-center text-gray-500">No results found</div>
       ) : (
